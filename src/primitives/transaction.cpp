6ade1939e3cfa8fdba8f28c5cbe69ed602444c3b
Matt Corallo <git@bluematt.me>
2016-09-30 17:01:53 -0400

    Fix performance regression due to segwit not caching withashes

--- src/primitives/transaction.cpp
+++ src/primitives/transaction.cpp
@@ -74,6 +74,8 @@ void CTransaction::UpdateHash() const
 
 uint256 CTransaction::GetWitnessHash() const
 {
+    if (wit.IsNull())
+        return GetHash();
     return SerializeHash(*this, SER_GETHASH, 0);
 }
 
